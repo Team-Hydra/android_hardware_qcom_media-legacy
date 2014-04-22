21875b8a099059cd15b4aec24d28be2e20606437
Dorian Snyder <dastin1015@gmail.com>
2014-04-21 21:39:56 -0700

    mm-video: port heap mask change to ion.
    Change-Id: Ifcb1316b5c26d5a1d2d490d177140057b3e93e16
    
    Gopikrishnaiah Anandan

--- mm-video/vidc/venc/src/video_encoder_device.cpp
+++ mm-video/vidc/venc/src/video_encoder_device.cpp
@@ -1232,11 +1232,12 @@ OMX_U32 venc_dev::pmem_allocate(OMX_U32 size, OMX_U32 alignment, OMX_U32 count)
       return -1;
   }
 
+  recon_buff[count].alloc_data.flags = 0;
   recon_buff[count].alloc_data.len = size;
 #ifdef MAX_RES_720P
-  recon_buff[count].alloc_data.flags = ION_HEAP(MEM_HEAP_ID);
+  recon_buff[count].alloc_data.heap_mask = ION_HEAP(MEM_HEAP_ID);
 #else
-  recon_buff[count].alloc_data.flags = (ION_HEAP(MEM_HEAP_ID) |
+  recon_buff[count].alloc_data.heap_mask = (ION_HEAP(MEM_HEAP_ID) |
                                         ION_HEAP(ION_IOMMU_HEAP_ID));
 #endif
   recon_buff[count].alloc_data.align = clip2(alignment);
